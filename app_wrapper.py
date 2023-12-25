948e59517a072b3ba6ca43f76fe9a022e34d5e8e
Siddharth Krishnan <sidkrishnan@protonmail.com>
2023-12-25 13:29:12 +0530

    Update app_wrapper.py

--- app_wrapper.py
+++ app_wrapper.py
@@ -9,16 +9,16 @@ KV = '''
 MDScreen:
 
     MDTextField:
-        id: text_input
-        hint_text: "Enter URL here"
-        pos_hint: {'center_x': 0.5, 'center_y': 0.7}
-        size_hint_x: 0.6
+        id : text_input
+        hint_text : "Enter URL here"
+        pos_hint : {'center_x': 0.5, 'center_y': 0.7}
+        size_hint_x : 0.6
 
     MDRectangleFlatButton:
-        id: submit_button
-        text: "DOWNLOAD"
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-
+        id : submit_button
+        text : "DOWNLOAD"
+        pos_hint : {'center_x': 0.5, 'center_y': 0.5}
+        on_press : md_bg_color = (1.0, 0.0, 0.0, 1.0)
 '''
 
 class MyApp(MDApp):
@@ -26,17 +26,20 @@ class MyApp(MDApp):
     title = "YouTube Downloader"
 
     def build(self):
+
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+
         self.my_utils = YouTubeDownloader()
         screen = Builder.load_string(KV)
 
         screen.ids.submit_button.bind(on_press=self.handle_button_click)
-        # screen.ids.call_method_button.bind(on_press=self.my_utils.download_video)
 
         return screen
 
     def handle_button_click(self, instance):
         text = self.root.ids.text_input.text  # Get the input text
-        self.my_utils.download_video(text)  # Pass the text to the external method
+        self.my_utils.download_video(text)  
         self.show_popup()
     
     def show_popup(self):
